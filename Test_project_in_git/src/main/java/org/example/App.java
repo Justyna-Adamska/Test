7ba009bddff683701f50b1275a0b79d35fdf3ab1
Justyna Adamska <110631335+Justyna-Adamska@users.noreply.github.com>
2023-04-18 20:40:07 +0200

    Update App.java

--- Test_project_in_git/src/main/java/org/example/App.java
+++ Test_project_in_git/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World123456789!" );
+        System.out.println( "Hello World12345hello!" );
     }
 }
